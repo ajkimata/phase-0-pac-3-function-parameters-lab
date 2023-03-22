310ee1e5b2f9a4853154d567481040cdaa4229b1
alfred kimata <alfyjason@gmail.com>
2023-03-22 05:08:19 +0300

    Functions passed successfully

---
+++ index.js
@@ -0,0 +1,12 @@
+function introduction(kimata){
+    return(`Hi, my name is ${kimata}.`);
+}
+function introductionWithLanguage(kimata,Javascript){
+    return(`Hi, my name is ${kimata} and I am learning to program in ${Javascript}.`);
+}
+function introductionWithLanguageOptional(name, language="JavaScript"){
+    return(`Hi, my name is ${name} and I am learning to program in ${language}.`);
+}
+function introductionWithLanguageOptional(name, language="JavaScript"){
+    return(`Hi, my name is ${name} and I am learning to program in ${language}.`);
+}
